a6603e80ce9b4d4b3c689dc3fad83e3afe3353e0
unknown <demeteraron0427@gmail.com>
2024-03-06 14:09:47 +0100

    deployed frontend and made the requierd changes for it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import PortfolioOverview from './components/PortfolioOverview';
 import StockDetail from './components/StockDetail';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // i should have hash router instead of browser router
 
 function App() {
   const [selectedStock, setSelectedStock] = useState(null);
 
   return (
     <Router>
-      <Routes> {/* Replace Switch with Routes */}
-        <Route exact path="/" element={<PortfolioOverview onStockSelect={setSelectedStock} />} /> {/* Update Route syntax */}
-        <Route path="/stock/:symbol" element={<StockDetail stock={selectedStock} />} /> {/* Update Route syntax */}
+      <Routes>
+        <Route exact path="/" element={<PortfolioOverview onStockSelect={setSelectedStock} />} />
+        <Route path="/stock/:symbol" element={<StockDetail stock={selectedStock} />} />
       </Routes>
     </Router>
   );
