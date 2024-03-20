3e3c1765613c8f74fd04abb9c9a77449674fc61d
arondemeter1 <arondemeter@student.ie.edu>
2024-03-20 12:04:02 +0100

    comment fix

--- frontend/src/components/StockDetail.js
+++ frontend/src/components/StockDetail.js
@@ -15,7 +15,7 @@ function StockDetail() {
   //define fetchStockDetails as a callback function so it can be re-used and passed around if needed
   const fetchStockDetails = useCallback(() => {
     setLoading(true);
-    axios.get(`https://mcsbt-integration-arondemeter.ey.r.appspot.com/stock/${symbol}`) // Update this at deployment
+    axios.get(`https://mcsbt-integration-arondemeter.ey.r.appspot.com/stock/${symbol}`) // update this at deployment
       .then(response => {
         setStockDetails(response.data);
         setError(null); // Clear any previous errors
